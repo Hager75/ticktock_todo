ad1ef3703247e672b291dbcad88ca7ca7cbd3459
Hager75 <58911651+Hager75@users.noreply.github.com>
2024-07-28 02:19:07 +0300

    feat: add test cases for login (#7)
    Co-authored-by: hagar.mohamed <hmohamed-v@ejada.com>

--- src/features/AuthLayout/Login/Login.tsx
+++ src/features/AuthLayout/Login/Login.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
